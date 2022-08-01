6f594618cba4653bce000ecb789fbb7f4ade126b
7956560430 <7956560430@pcke90007m.ecc.u-tokyo.ac.jp>
2022-08-01 16:59:56 +0900

    Change Name of Theorems

--- GeoLean/Ch02.lean
+++ GeoLean/Ch02.lean
@@ -2,31 +2,32 @@ import GeoLean
 open TarskiAxioms
 variable (t:TarskiAxioms)
 
-theorem eq_refl : ∀ A B:Point, δ A B A B := by
+theorem cong_reflexivity : ∀ A B:Point, δ A B A B := by
 intro A B
 apply transEq B A A B
 apply And.intro
 . apply reflEq
 . apply reflEq
 
-theorem eq_symmetry : ∀ A B C D:Point, δ A B C D → δ C D A B := by
+theorem cong_symmetry : ∀ A B C D:Point, δ A B C D → δ C D A B := by
 intro A B C D dabcd
 apply transEq A B
 apply And.intro
 . exact dabcd
-. apply eq_refl
+. apply cong_reflexivity
 
-theorem eq_trans : ∀ A B C D E F, δ A B C D → δ C D E F → δ A B E F := by
+theorem cong_transitivity : ∀ A B C D E F, δ A B C D → δ C D E F → δ A B E F := by
 intro A B C D E F dabcd dcdef
 apply transEq C D
 apply And.intro
-. apply eq_symmetry
+. apply cong_symmetry
   exact dabcd
 . exact dcdef
 
-theorem eq_left_commutative : ∀ A B C D, δ A B C D → δ B A C D := by
+theorem cong_left_commutativity : ∀ A B C D, δ A B C D → δ B A C D := by
 intro A B C D dabcd
-apply eq_trans t B A A B
+apply cong_transitivity t B A A B
 . apply reflEq
 . exact dabcd
+
 
